7beb8a4f35dc7494e9424074fd8f9891f5a7a4e5
patrick <patrick.ndongo@movingimage.com>
2016-01-19 11:25:29 +0100

    add express index

---
+++ web.js
@@ -0,0 +1,11 @@
+/**
+ * Created by patrick on 19.01.16.
+ */
+var express = require('express');
+var serve = express();
+
+serve.use(express.static(__dirname + '/web'));
+serve.set('port', (process.env.PORT || 9876));
+serve.listen(serve.get('port'), function () {
+    console.log('Node app of sample-angular is running at localhost:' + serve.get('port'));
+});
